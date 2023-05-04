8c6eeccf87779822d12477734c8e33df26e56d7c
Maiatskaia Natalia <mia@pop-os.localdomain>
2023-05-05 02:05:32 +0600

    Shortened DATABASE list in settings.py

--- neo4UrlShort/settings.py
+++ neo4UrlShort/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,15 +77,11 @@ WSGI_APPLICATION = 'neo4UrlShort.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DEBUG = config('DEBUG', cast=bool)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': '',
-    }
+DATABASES = {'default': dj_database_url.config(
+    default=config("DATABASE_URL"),
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 }
 
 # Password validation
